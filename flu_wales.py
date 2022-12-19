87cfce6e67e7069fa7fe7ca33720b8335b6d2b51
SallySamNIHR <87127667+SallySamNIHR@users.noreply.github.com>
2022-12-19 16:00:19 +0000

    Update flu_wales.py

--- flu_wales.py
+++ flu_wales.py
@@ -6,9 +6,7 @@ def save_url(url):
 
 
 if __name__ == '__main__':
-    data = save_url("https://www2.nphs.wales.nhs.uk/CommunitySurveillanceDocs.nsf/3dc04669c9e1eaa880257062003b246b"
-                    "/023d7e78efdbcc3980258917005779d4/$FILE/Weekly%20ARI%20hospital%20dashboard%20data%20-%20last"
-                    "%2090%20days.xlsx")
+    data = save_url("https://www2.nphs.wales.nhs.uk/CommunitySurveillanceDocs.nsf/3dc04669c9e1eaa880257062003b246b/023d7e78efdbcc3980258917005779d4/$FILE/Weekly%20ARI%20hospital%20dashboard%20data%20-%20last%2090%20days.xlsx")
     out_path = "test_wales.xlsx"
 
     with pd.ExcelWriter(
