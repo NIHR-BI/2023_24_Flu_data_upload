223b7afefc7325b5fa9ffc8e10857651cedc90e2
SallySamNIHR <87127667+SallySamNIHR@users.noreply.github.com>
2022-12-19 15:18:13 +0000

    Update flu.py

--- flu.py
+++ flu.py
@@ -1,17 +1,18 @@
 import pandas as pd
 
-url = "https://www2.nphs.wales.nhs.uk/CommunitySurveillanceDocs.nsf/3dc04669c9e1eaa880257062003b246b/023d7e78efdbcc3980258917005779d4/$FILE/Weekly%20ARI%20hospital%20dashboard%20data%20-%20last%2090%20days.xlsx"
 
-a = pd.read_excel(url)
+def save_url(url):
+    return pd.read_excel(url)
 
-# a.to_excel('test.xlsx')
 
-# out_path = "test.xlsx"
+if __name__ == '__main__':
+    data = save_url("https://www2.nphs.wales.nhs.uk/CommunitySurveillanceDocs.nsf/3dc04669c9e1eaa880257062003b246b"
+                    "/023d7e78efdbcc3980258917005779d4/$FILE/Weekly%20ARI%20hospital%20dashboard%20data%20-%20last"
+                    "%2090%20days.xlsx")
+    out_path = "test.xlsx"
 
-# out_path = "/Users/ssam/PycharmProjects/flu/test/test.xlsx"
-
-# with pd.ExcelWriter(
-#         path=out_path,
-#         mode="w",
-#         engine="xlsxwriter") as writer:
-#     a.to_excel(writer, sheet_name="Sheet1")
+    with pd.ExcelWriter(
+            path=out_path,
+            mode="w",
+            engine="xlsxwriter") as writer:
+        data.to_excel(writer, sheet_name="Sheet1")
