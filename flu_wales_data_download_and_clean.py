b965eb953d7f064efa740024701ac6794b9734f7
Sally Sam <sally.sam@nihr.ac.uk>
2023-10-16 15:15:18 +0100

    automate Wales to run on desktop then push

--- flu_wales_data_download_and_clean.py
+++ flu_wales_data_download_and_clean.py
@@ -1,3 +1,4 @@
+import requests
 import pandas as pd
 
 
@@ -40,14 +41,19 @@ def clean(df: object, sheet_names: list) -> object:
 
 
 if __name__ == '__main__':
-    # save data
+    # download data
+    url = "https://www2.nphs.wales.nhs.uk/CommunitySurveillanceDocs.nsf/3dc04669c9e1eaa880257062003b246b/023d7e78efdbcc3980258917005779d4/$FILE/Weekly%20ARI%20hospital%20dashboard%20data%20-%20last%2090%20days.xlsx"
+    url_response = requests.get(url)
+    data = url_response.content
+
+    out_path = "Weekly ARI hospital dashboard data - last 90 days.xlsx"
+    with open(out_path, 'wb') as file:
+        file.write(data)
+
+    # clean data
     sheets = ["Flu com acq cases adm to hosp", "Flu com acq cases adm to CC",
               "Flu hosp cases by acq", "Flu hosp cases adm to CC by acq",
               "Flu hosp IP cases", "Flu CC IP cases"]
 
     clean_data = clean(load_data(sheets), sheets)
-
-    with open('wales_flu_data.csv', 'w+') as f:
-        f.write(clean_data.to_csv(index=False))
-
-    # print(1404*3 + 117*3 + 1404*3 + 1404 + 117 + 117)
+    clean_data.to_csv('wales_flu_data.csv', index=False)
