21827d620150fc55bc9b554b4dafe548c1ce47b5
Sally Sam <sally.sam@nihr.ac.uk>
2023-12-11 12:42:10 +0000

    incl england data

--- flu_england.py
+++ flu_england.py
@@ -5,16 +5,16 @@ from bs4 import BeautifulSoup
 
 def retrieve_first_url(webpage, text_in_url):
     data = requests.get(url=webpage)
-    data_soup = BeautifulSoup(data.content, 'html.parser')
+    data_soup = BeautifulSoup(data.content, "html.parser")
 
     # find all <a href> references
     for link in data_soup.find_all("a"):
         # Retrieve the first URL with the text bellow
-        if link['href'].find(text_in_url) >-1 : 
+        if link["href"].find(text_in_url) > -1:
             # Use this link to scrape
-            linkToScrape = link['href']
+            linkToScrape = link["href"]
             return linkToScrape
-        
+
 
 def is_even(value):
     if value % 2 == 0:
@@ -30,11 +30,11 @@ def fill_from_cell_before(df, row, col_before):
 
 def fill_merged_dates(df, row, col_start):
     """This assumes that the dates start from column 5 and the date is in the odd columns and there are two columns
-    per date """
+    per date"""
     max_cols = len(df.columns)
     for col in range(col_start, max_cols + 1):
         if is_even(col):
-            fill_from_cell_before(df, row, col-1)
+            fill_from_cell_before(df, row, col - 1)
     return df
 
 
@@ -60,10 +60,20 @@ def remove_cols_containing_text(df, col, text):
     return df[df[col] != text]
 
 
-def clean(df: object, na_col1: int, date_row: int, date_col_start: int, columns_to_remove: int,
-          header_rows: list, index_col_names: list, na_col2: int,
-          remove_col1: int, remove_text1: str,
-          remove_col2: int, remove_text2: str) -> object:
+def clean(
+    df: object,
+    na_col1: int,
+    date_row: int,
+    date_col_start: int,
+    columns_to_remove: int,
+    header_rows: list,
+    index_col_names: list,
+    na_col2: int,
+    remove_col1: int,
+    remove_text1: str,
+    remove_col2: int,
+    remove_text2: str,
+) -> object:
     fill_merged_dates(df, date_row, date_col_start)
     df = remove_column(df, columns_to_remove)
     df = set_index_cols(df, index_col_names)
@@ -77,32 +87,41 @@ def clean(df: object, na_col1: int, date_row: int, date_col_start: int, columns_
     return df
 
 
-if __name__ == '__main__':
-    url_to_use = retrieve_first_url("https://www.england.nhs.uk/statistics/statistical-work-areas/uec-sitrep/urgent-and-emergency-care-daily-situation-reports-2022-23/",
-                                    "UEC-Daily-SitRep")
-    
+if __name__ == "__main__":
+    url_to_use = retrieve_first_url(
+        "https://www.england.nhs.uk/statistics/statistical-work-areas/uec-sitrep/urgent-and-emergency-care-daily-situation-reports-2023-24/",
+        "UEC-Daily-SitRep",
+    )
+
+    # "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2023/12/Web-File-Timeseries-UEC-Daily-SitRep.xlsm"
+
     data = pd.read_excel(
-        io=url_to_use,
-        sheet_name="Flu",
-        header=None,
-        usecols="B:XFD",
-        skiprows=13)
+        io=url_to_use, sheet_name="Flu", header=None, usecols="B:XFD", skiprows=13
+    )
 
-    clean_data = clean(df=data,
-                       na_col1=5,
-                       date_row=0,
-                       date_col_start=5,
-                       columns_to_remove=2,
-                       index_col_names=[1, 3, 4],
-                       header_rows=[0, 1],
-                       na_col2=3,
-                       remove_col1=3,
-                       remove_text1='Code',
-                       remove_col2=3,
-                       remove_text2='-'
-                       )
+    clean_data = clean(
+        df=data,
+        na_col1=5,
+        date_row=0,
+        date_col_start=5,
+        columns_to_remove=2,
+        index_col_names=[1, 3, 4],
+        header_rows=[0, 1],
+        na_col2=3,
+        remove_col1=3,
+        remove_text1="Code",
+        remove_col2=3,
+        remove_text2="-",
+    ).dropna(how="all")
 
-    clean_data.columns = ['region', 'trust_code', 'trust_name', 'date', 'measure', 'value']
+    clean_data.columns = [
+        "region",
+        "trust_code",
+        "trust_name",
+        "date",
+        "measure",
+        "value",
+    ]
 
-    with open('england_flu_data.csv', 'w+') as f:
+    with open("england_flu_data.csv", "w+") as f:
         f.write(clean_data.to_csv(index=False))
